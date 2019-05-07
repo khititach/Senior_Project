05cf3097bd2f84d9baa6ad3da065aa6493b4c47c
khititach <khititachk58@email.nu.ac.th>
2019-05-07 22:32:49 +0700

    commit 1

---
+++ Login/login.js
@@ -0,0 +1,11 @@
+// var express = require('express');
+// var routes = express.Router();
+
+// app.get('/home', function(request, response) {
+// 	if (request.session.loggedin) {
+// 		response.send('Welcome back, ' + request.session.username + '!');
+// 	} else {
+// 		response.send('Please login to view this page!');
+// 	}
+// 	response.end();
+// });
